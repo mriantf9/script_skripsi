20cb7b9b35ee02c7f64ea34996f16e3fd63e2a4a
mriantf9 <muhamad@riantf.id>
2020-12-20 18:03:11 +0700

    bismillah

--- realtime_script/rsyncRealtime.sh
+++ realtime_script/rsyncRealtime.sh
@@ -15,18 +15,22 @@ DIR="/home/mriantf/script_skripsi"
 WORKDIR="$DIR/script"
 TMP="$WORKDIR/tmp"
 UNIQ=$1
+FILENAME=$2
 DTL="${DIR}/REALTIME/data_list"
 
 QUEUE="/var/www/html/skripsi/storage/app/realtime_task"
 
 #rm -rf ${DIR}/REALTIME_QUEUE/*
 
-cp ${QUEUE}/* ${DIR}/REALTIME_QUEUE/
+mv ${QUEUE}/${FILENAME} ${DIR}/REALTIME_QUEUE/${FILENAME}
 
-ls ${DIR}/REALTIME_QUEUE > ${DIR}/REALTIME_QUEUE/tmp_list
+ls ${DIR}/REALTIME_QUEUE > ${DIR}/REALTIME_QUEUE/tmp/tmp_list
 
 for i in `cat ${DIR}/REALTIME_QUEUE/tmp_list`
 do
     echo "Process Rsync ${i}"
     /usr/bin/rsync -r -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22" ${DIR}/REALTIME_QUEUE/${i}  mriantf@27.131.3.177:${DTL}
 done
+
+rm -rf ${DIR}/REALTIME_QUEUE/tmp/tmp_list
+
