fd226eef9050a1a015126f18ff973b726fc2ef0a
mriantf9 <muhamad@riantf.id>
2020-12-20 18:42:13 +0700

    revisi

--- realtime_script/rsyncRealtime.sh
+++ realtime_script/rsyncRealtime.sh
@@ -13,24 +13,25 @@ my_touch() {
 
 DIR="/home/mriantf/script_skripsi"
 WORKDIR="$DIR/script"
-TMP="$WORKDIR/tmp"
 UNIQ=$1
 FILENAME=$2
 DTL="${DIR}/REALTIME/data_list"
 
 QUEUE="/var/www/html/skripsi/storage/app/realtime_task"
 
-#rm -rf ${DIR}/REALTIME_QUEUE/*
+rm -rf ${DIR}/REALTIME_QUEUE/data_list/*
 
-mv ${QUEUE}/${FILENAME} ${DIR}/REALTIME_QUEUE/${FILENAME}
+#mv ${QUEUE}/${FILENAME} ${DIR}/REALTIME_QUEUE/${FILENAME}
 
-ls ${DIR}/REALTIME_QUEUE > ${DIR}/REALTIME_QUEUE/tmp/tmp_list
+ls ${QUEUE} > ${DIR}/REALTIME_QUEUE/tmp_list
 
 for i in `cat ${DIR}/REALTIME_QUEUE/tmp_list`
 do
+    echo "moving file ${i} into ${DIR}/REALTIME_QUEUE/data_list/${i}"
+    mv ${QUEUE}/${i} ${DIR}/REALTIME_QUEUE/data_list/${i}
     echo "Process Rsync ${i}"
-    /usr/bin/rsync -r -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22" ${DIR}/REALTIME_QUEUE/${i}  mriantf@27.131.3.177:${DTL}
+    /usr/bin/rsync -r -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22" ${DIR}/REALTIME_QUEUE/data_list/${i}  mriantf@27.131.3.177:${DTL}
 done
 
-rm -rf ${DIR}/REALTIME_QUEUE/tmp/tmp_list
+rm -rf ${DIR}/REALTIME_QUEUE/tmp_list
 
